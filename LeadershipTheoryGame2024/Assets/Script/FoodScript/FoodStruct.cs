729ee3bfcb21b31169266847d4a45fb5a79cbab8
展鹏 陈 <kekeboys@126.com>
2024-12-07 19:00:25 +0900

    更新了一些测试文件，FoodStruct，CookStruct，TheGameTool，Fish_GetFood

--- LeadershipTheoryGame2024/Assets/Script/FoodScript/FoodStruct.cs
+++ LeadershipTheoryGame2024/Assets/Script/FoodScript/FoodStruct.cs
@@ -5,18 +5,17 @@ public struct FoodStruct
 {
     public Image image;           //食物图片
     public bool isUseTool;        //是否使用工具
-    public bool isCooked;         //是否进行过烹饪处理
     public bool isCombine;        //是否可以和其他食物结合
     public string foodName;       //食物名称
     public int foodID;            //食物ID
-
-    public FoodStruct(Image image, bool isUseTool, bool isCooked, bool isCombine, string foodName, int foodID)
+    public CooklStruct cs;        //是否可烹制
+    public FoodStruct(Image image, bool isUseTool, bool isCombine, string foodName, int foodID, CooklStruct cs)
     { 
         this.image = image;
         this.isUseTool = isUseTool;
-        this.isCooked = isCooked;
         this.isCombine = isCombine;
         this.foodName = foodName;
         this.foodID = foodID;
+        this.cs = cs;
     }
 }
